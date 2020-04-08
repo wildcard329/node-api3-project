d15e28a060d80eccd189dd8cecc5ecf7f14c6a87
wildcard329 <enkiduwildman@gmail.com>
2020-04-08 14:49:42 -0700

    all user endpoints except add post are working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,59 @@
 const express = require('express');
+const db = require('./postDb.js');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  db.get(req.query)
+  .then(post => {
+    res.status(200).json(post);
+  })
+  .catch(error => {
+    console.error(error.message);
+    res.status(500).json({
+      message: 'Error retrieving the posts',
+    })
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  db.getById(req.params.id)
+  .then(post => {
+    if(post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'Post not fount' });
+    }
+  })
+  .catch(error => {
+    console.error(error.message);
+    res.status(500).json({
+      message: 'Error retrieving the post',
+    });
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  db.remove(req.params.id)
+  .then(count => {
+    if (count > 0) {
+      res.status(200).json({ message: 'The post has been removed' });
+    } else {
+      res.status(404).json({ message: 'The post could not be found' });
+    }
+  })
+  .catch()
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  db.update(req.params.id, req.body)
+  .then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(400).json({ message: 'The post could not be found' })
+    }
+  })
 });
 
 // custom middleware
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,106 @@
 const express = require('express');
+const db = require('./userDb.js');
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  // do your magic!
+  db.insert(req.body)
+  .then(user => {
+    res.status(201).json(user);
+  })
+  .catch(error => {
+    console.error(error.message);
+    res.status(500).json({
+      message: 'Error adding the user',
+    })
+  })
 });
 
 router.post('/:id/posts', (req, res) => {
-  // do your magic!
+  const messageInfo = {...req.body, user_id: req.params.id}
+  
+  db.insert(messageInfo)
+  .then(message => {
+    res.status(200).json(message);
+  })
+  .catch(error => {
+    console.error(error.message);
+    res.status(500).json({
+      message: 'Error adding message'
+    })
+  })
+
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  db.get(req.query)
+  .then(user => {
+    res.status(200).json(user);
+  })
+  .catch(error => {
+    console.error(error.message);
+    res.status(500).json({
+      message: 'Error retrieving the users',
+    })
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  db.getById(req.params.id)
+  .then(user => {
+    if(user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: 'User not fount' });
+    }
+  })
+  .catch(error => {
+    console.error(error.message);
+    res.status(500).json({
+      message: 'Error retrieving the user',
+    });
+  });
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  db.getUserPosts(req.params.id)
+  .then(posts => {
+    res.status(200).json(posts);
+  })
+  .catch(error => {
+    console.error(error.message);
+    res.status(200).json({
+      message: 'Error getting user posts'
+    })
+  })
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  db.remove(req.params.id)
+  .then(count => {
+    if (count > 0) {
+      res.status(200).json({ message: 'The user has been deleted' })
+    } else {
+      res.status(400).json({ message: 'The user could not be fount' })
+    }
+  })
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  db.update(req.params.id, req.body)
+  .then(user => {
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: 'The user could not be found' })
+    }
+  })
+  .catch(error => {
+    console.error(error.message);
+    res.status(500).json({
+      message: 'Error updating the user' 
+    })
+  })
 });
 
 //custom middleware
